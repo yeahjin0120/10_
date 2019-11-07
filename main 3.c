6a4fc603f171e247d543d4f8b226c29eac1ba505
yeahjin0120 <55428346+yeahjin0120@users.noreply.github.com>
2019-11-07 16:11:32 +0900

    04

--- main 3.c
+++ main 3.c
@@ -3,19 +3,6 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void swap (int x, int y){
-	int temp;
-	temp = x;
-	x =y;
-	y = temp;
-}
 
-void main(void){
-	int a =3;
-	int b =5;
-	swap (a,b);
-	printf("a:%i , b:%i\n", a,b);
-}
-	
 	return 0;
 }
